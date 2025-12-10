ccb6fc359838cc8f07e518f6fc32ef71ffd56ced
Davi120906 <163910228+Davi120906@users.noreply.github.com>
2025-12-10 16:11:52 -0300

    subindo a base

---
+++ HostWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ChatP2P;
+    public partial class HostWindow : Window
+    {
+        public HostWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
---
+++ MainWindow.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia.Controls;
+
+namespace ChatP2P;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        BtnServidor.Click += BtnServidor_Click;
+        BtnCliente.Click += BtnCliente_Click;
+    }
+    private void BtnServidor_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var hostWindow = new HostWindow();
+        hostWindow.Show();
+        this.Close();
+    }
+    private void BtnCliente_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+
+    }
+}
